74bca72f7d268fe84613799dbe0f3bef06696938
Messias Fernandes <66951793+messiasfernandes@users.noreply.github.com>
2024-06-03 23:08:58 -0300

    mesa com total por comanda

--- src/app/comandas/mesacomandas/mesacomandas.component.ts
+++ src/app/comandas/mesacomandas/mesacomandas.component.ts
@@ -9,7 +9,8 @@ import { ComandaService } from 'src/app/services/comanda.service';
 export class MesacomandasComponent implements OnInit {
   layout: string = 'list';
   mesas: any[] = [];
-  total: number = 0;
+  total:number=0
+
   constructor(private comandaService: ComandaService){}
   ngOnInit(): void {
     this.getMesa;
@@ -20,20 +21,34 @@ export class MesacomandasComponent implements OnInit {
  this.comandaService.pesquisar().subscribe((dados: any) => {
   console.log(dados);
   this.mesas = dados;
-this.calculateTotal();
-
+ this.calculateTotal();
 });
-console.log(this.mesas)
-
-  }
+}
   calculateTotal(): void {
-    this.total =0;
     this.mesas.forEach(mesa => {
-      mesa.total = mesa.comandas.reduce((acc, comanda) => acc + comanda.total, 0);
-      this.total += mesa.total;
+      console.log(`Capacidade da mesa: ${mesa.capacidade}`);
+      console.log(`ID da mesa: ${mesa.id}`);
+      console.log(`Localização da mesa: ${mesa.loclizacao}`);
+      console.log(`Número da mesa: ${mesa.numerodaMesa}`);
+      console.log(`Status da mesa: ${mesa.statusMesa}`);
+
+      // Inicializa o total das comandas para esta mesa
+   //  mesa.totalComandas = 0;
+
+      mesa.comandas.forEach((comanda, index) => {
+        console.log(`Comanda ${index + 1}:`);
+        console.log(`  ID: ${comanda.id}`);
+        console.log(`  Data de Abertura: ${comanda.data_abertura}`);
+        console.log(`  Status de Pagamento: ${comanda.statusPagamentoComanda}`);
+        console.log(`  Total: ${comanda.total}`);
+        console.log(`  Itens da Comanda: ${comanda.itemsdaComanda.length}`);
+
+        // Atualiza o total das comandas para esta mesa
+        mesa.totalComandas += comanda.total;
+      });
+      this.total= mesa.totalComandas
+      // Exibe o total das comandas para esta mesa
+      console.log(`Total das Comandas da Mesa ${mesa.id}: ${mesa.totalComandas}`);
     });
-
   }
-
-
 }
